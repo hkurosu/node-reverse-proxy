99c46de328f46e6a9e571503469ada141ed28fea
Hiro Kurosu <hkurosu@gmail.com>
2016-05-25 11:43:29 -0700

    make TLS proxy with termination

--- RP-ssl-term.js
+++ RP-ssl-term.js
@@ -4,21 +4,38 @@ var httpProxy = require('http-proxy'),
 
 // tls proxy port
 var tlsPort = 5053;
-// https proxy
+// hostname white list
+var hostNames = ['localhost', 'lw5-hku1-bio.dsone.3ds.com'];
+
+function fixRedirectUrl(proxyRes) {
+  if (proxyRes.headers['location']) {
+    var loc = url.parse(proxyRes.headers['location']);
+    if (loc.protocol == 'http:' && loc.port == tlsPort) {
+      if (hostNames.indexOf(loc.hostname) >= 0) {
+        loc.protocol = 'https:';
+        proxyRes.headers['location'] = loc.format();
+        console.log('### http -> https: ' + loc.protocol + loc.host + loc.path + ' ...');
+      }
+    }
+  }
+}
+
+// https proxy (https -> http)
 var tlsProxy = httpProxy.createProxyServer({
-  target: 'https://localhost:8443',
+  target: 'http://localhost:8080',
   ssl: {
     key: fs.readFileSync('key.pem', 'utf8'),
     cert: fs.readFileSync('cert.pem', 'utf8'),
     passphrase: 'changeme'
   },
-  secure: false,
+  // secure: false,
   // xfwd: true,
-  // autoRewrite: true,
-  // protocolRewrite: 'https'
 }).listen(tlsPort);
 tlsProxy.on('proxyRes', function(proxyRes, req, res) {
   console.log('req: ' + req.url);
+
+  // hack location header
+  fixRedirectUrl(proxyRes);
   
   if (!req.url.match(/\/(assets\/|notifications)/)) {
     console.log('  req headers: ' + JSON.stringify(req.headers, true, 2));
--- createIt.bat
+++ createIt.bat
@@ -1,1 +1,2 @@
-openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 3650
+openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 ^
+  -subj "/C=US/ST=California/L=San Diego/O=BIOVIA/OU=R&D/CN=%1"
